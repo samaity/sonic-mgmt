f34b3c73fbc950dd1da118aa4c48d6c5c5c89d6c
Praveen Chaudhary <pchaudhary@linkedin.com>
2020-10-13 17:55:28 -0700

    [server6_STR-ACS-SERV-01.yml]: Store Server Vars files and a copy script.
    Changes:
    -- Store Server Vars files.
    -- a copy script.
    
    Signed-off-by: Praveen Chaudhary<pchaudhary@linkedin.com>
    RB=2326858
    G=lnos-reviewers
    R=pchaudhary,pmao,rmolina,samaity,sfardeen,zxu
    A=

---
+++ ansible/linkedin/copy_testbed2.sh
@@ -0,0 +1,8 @@
+cp ~/sonic-mgmt/ansible/linkedin/dut04_inventory ~/sonic-mgmt/ansible/inventory
+cp ~/sonic-mgmt/ansible/linkedin/dut04_lab ~/sonic-mgmt/ansible/lab
+cp ~/sonic-mgmt/ansible/linkedin/dut04_lab_connection_graph.xml ~/sonic-mgmt/ansible/files/lab_connection_graph.xml
+cp ~/sonic-mgmt/ansible/linkedin/dut04_sonic_lab_devices.csv ~/sonic-mgmt/ansible/files/sonic_lab_devices.csv
+cp ~/sonic-mgmt/ansible/linkedin/dut04_sonic_lab_links.csv ~/sonic-mgmt/ansible/files/sonic_lab_links.csv
+
+cp ~/sonic-mgmt/ansible/linkedin/server8_veos ~/sonic-mgmt/ansible/veos
+cp ~/sonic-mgmt/ansible/linkedin/server8_STR-ACS-SERV-01.yml ~/sonic-mgmt/ansible/host_vars/STR-ACS-SERV-01.yml
